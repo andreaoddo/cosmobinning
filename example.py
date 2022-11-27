75de75856ed53a4f8923544a1abcb7776ac2bff8
andreaoddo <andrea.oddo89@gmail.com>
2022-11-27 17:15:36 +0100

    :new: Included example of redshift space binning

--- example.py
+++ example.py
@@ -1,11 +1,15 @@
-from cosmobinning.lib import Bins, RealSpacePowerBinner
+from cosmobinning.lib import Bins, RealSpacePowerBinner, RedshiftSpacePowerBinner
 import matplotlib.pyplot as plt
 import numpy as np
+from time import time
 
 
 def func(x):
 	return x ** 2 - x + 1
 
+
+def anis_func(x, _mu):
+	return x ** 2 - x + 1
 
 bins = Bins.linear_bins(1.0, 1.0, 128)
 binner = RealSpacePowerBinner(bins)
@@ -19,7 +23,11 @@ y_bin = binner.bin_function(func)
 x_eff = binner.bin_function(lambda x: x)
 y_eff = func(x_eff)
 
-plt.plot(x_bin, 100 * np.abs(1 - y_eff / y_bin), 'o')
+z_binner = RedshiftSpacePowerBinner(bins)
+y_rsd = z_binner.bin_function(anis_func, 0)
+
+plt.semilogy(x_bin, np.abs(1 - y_eff / y_bin), 'o')
+plt.semilogy(x_bin, np.abs(1 - y_rsd / y_bin), 'o')
 plt.xlabel('x')
-plt.ylabel('Relative error of effective [%]')
+plt.ylabel('Relative error to real space binned')
 plt.show()
