18a89e51dd16184912b0b64cfc2695a99b8a6009
andreaoddo <andrea.oddo89@gmail.com>
2023-11-15 23:46:59 +0100

    :new: Added effective binning for real space bispectrum

--- lib.py
+++ lib.py
@@ -3,13 +3,14 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from enum import Enum, auto
 from warnings import warn
-from time import sleep
+from time import sleep, time
 
-from numpy import ndarray, int16, int32, float64, arange, linspace, digitize, bincount, meshgrid, fromiter, flip, fromfunction, zeros, divide, array, absolute, logspace
+from numpy import ndarray, int16, int32, float64, arange, linspace, digitize, bincount, meshgrid, fromiter, flip, fromfunction, zeros, divide, array, absolute, logspace, loadtxt
 from numpy import sqrt as npsqrt
 from numpy import sum as npsum
-from typing import Callable, Optional
+from typing import Callable, Optional, Tuple
 from math import isqrt, isclose, log10, factorial
+import pathlib
 from scipy.special import eval_legendre
 from scipy.misc import derivative
 from scipy.integrate import simpson
@@ -18,7 +19,7 @@ from sys import getsizeof
 
 
 class Bin:
-	def __init__(self, inf: float, sup: float) -> None:
+	def __init__(self, inf: float, sup: float, right_open: bool = True) -> None:
 		"""
 		Initializes a Bin object instance
 		:param inf: Inferior bound of the bin
@@ -28,6 +29,7 @@ class Bin:
 		"""
 		self.inf = inf
 		self.sup = sup
+		self.right_open = right_open
 
 	def __repr__(self) -> str:
 		"""
@@ -35,7 +37,10 @@ class Bin:
 		:return: String representation of the object
 		:rtype: str
 		"""
-		return f"[{self.inf},{self.sup})"
+		if self.right_open:
+			return f"[{self.inf},{self.sup})"
+		else:
+			return f"({self.inf},{self.sup}]"
 
 	def __eq__(self, other: Bin) -> bool:
 		"""
@@ -54,6 +59,9 @@ class Bin:
 		"""
 		return getsizeof(self.inf) + getsizeof(self.sup)
 
+	def __hash__(self):
+		return hash((self.inf, self.sup))
+
 	def center(self) -> float:
 		"""
 		Returns the central value of the bin.
@@ -62,9 +70,15 @@ class Bin:
 		"""
 		return 0.5 * (self.inf + self.sup)
 
+	def contains(self, k: float) -> bool:
+		if self.right_open:
+			return self.inf <= k < self.sup
+		else:
+			return self.inf < k <= self.sup
+
 
 class Bins:
-	def __init__(self, bins: list[Bin], right_open: bool = True) -> None:
+	def __init__(self, bins: list[Bin]) -> None:
 		"""
 		Initializes a Bins instance object
 		:param bins: A list of Bin objects
@@ -72,10 +86,13 @@ class Bins:
 		:param right_open: Indicates whether bins are right-open or not, defaults to True
 		:type right_open: bool
 		"""
+
+		assert all(map(lambda b: b.right_open, bins)) or all(map(lambda b: not b.right_open, bins))
+
 		for (i, b) in enumerate(bins[:-1]):
 			assert bins[i].sup == bins[i + 1].inf
 		self.bins = bins
-		self.right_open = right_open
+		self.right_open = bins[0].right_open
 
 	def __repr__(self):
 		"""
@@ -102,21 +119,15 @@ class Bins:
 		return list(map(lambda b: b.center(), self.bins))
 
 	@staticmethod
-	def linear_bins(first_center: float, width: float, bins: int, right_open: bool = True) -> Bins:
+	def linear_bins(binning_scheme: BinningScheme) -> Bins:
 		"""
 		Constructor static method to create a Bins instance object with linearly spaced bins with equal width
-		:param first_center: Center of the first bin
-		:type first_center: float
-		:param width: Width of each bin
-		:type width: float
-		:param bins: Number of bins
-		:type bins: int
-		:param right_open: Indicates whether bins are right-open or not, defaults to True
-		:type right_open: bool
+		:param binning_scheme: binning scheme from which the linear bins are created
+		:type binning_scheme: BinningScheme
 		:return: Bins instance object with linearly spaced bins
 		:rtype: Bins
 		"""
-		return Bins([Bin(first_center + (i - 0.5) * width, first_center + (i + 0.5) * width) for i in range(bins)], right_open = right_open)
+		return Bins([Bin(binning_scheme.first_center + (i - 0.5) * binning_scheme.width, binning_scheme.first_center + (i + 0.5) * binning_scheme.width, right_open=binning_scheme.right_open) for i in range(binning_scheme.bin_count)])
 
 	def edges(self) -> list[float]:
 		"""
@@ -199,6 +210,43 @@ class Bins:
 		for q2 in range(self.squared_max() + 1):
 			count_flipped = flip(count_grid[:q2 + 1])
 			yield 2 * count_flipped[squares[:isqrt(q2) + 1]].sum() - count_flipped[0]
+
+
+class BinningScheme:
+	def __init__(self, first_center: float, width: float, bin_count: int, right_open: bool = True):
+		self.first_center = first_center
+		self.width = width
+		self.bin_count = bin_count
+		self.right_open = right_open
+
+	def can_be_rebinned_into(self, other: BinningScheme) -> bool:
+		input_bins = Bins.linear_bins(self)
+		output_bins = Bins.linear_bins(other)
+
+		if self.right_open != other.right_open:
+			return False
+
+		width_ratio = other.width/self.width
+		if not width_ratio.is_integer() or width_ratio < 1:
+			return False
+
+		if (input_bins.bins[0].inf > output_bins.bins[0].inf) or (input_bins.bins[-1].sup < output_bins.bins[-1].sup):
+			return False
+
+		# For each output bin, there must be an input bin sharing the same inf and one sharing the same sup
+		for out_b in output_bins.bins:
+			check_inf = False
+			check_sup = False
+			for in_b in input_bins.bins:
+				check_inf = check_inf or (in_b.inf == out_b.inf)
+				check_sup = check_sup or (in_b.sup == out_b.sup)
+			if not(check_inf and check_sup):
+				return False
+
+		return True
+
+
+
 
 
 class PowerBinner:
@@ -510,3 +558,150 @@ class RedshiftSpacePowerExpansionBinner(Binner):
 		# power_eff_l has shape (der x inner-mult x bins)
 		# pi_dji has shape (der x inner-mult x ext-mult x bins)
 		return (2 * self.multipoles + 1)[:, None] * npsum(self.pi_dji * power_eff_l[:, :, None, :], axis=(0, 1))
+
+
+class TriangleBin:
+	def __init__(self, bin1: Bin, bin2: Bin, bin3: Bin) -> None:
+		assert bin1.right_open == bin2.right_open and bin2.right_open == bin3.right_open
+		self.bin1 = bin1
+		self.bin2 = bin2
+		self.bin3 = bin3
+
+	def __hash__(self):
+		return hash((self.bin1, self.bin2, self.bin3))
+
+	def __eq__(self, other):
+		return self.bin1 == other.bin1 and self.bin2 == other.bin2 and self.bin3 == other.bin3
+
+	def center(self) -> Tuple[float, float, float]:
+		return self.bin1.center(), self.bin2.center(), self.bin3.center()
+
+	def contains(self, triangle: Tuple[float, float, float]) -> bool:
+		k1, k2, k3 = triangle
+		return self.bin1.contains(k1) and self.bin2.contains(k2) and self.bin3.contains(k3)
+
+
+class TriangleBins:
+	def __init__(self, bins: list[TriangleBin], right_open: bool = True, open_triangles = True):
+		self.bins = bins
+		self.right_open = right_open
+		self.open_triangles = open_triangles
+
+	@staticmethod
+	def linear_bins(binning_scheme: BinningScheme, open_triangles: bool = True) -> TriangleBins:
+		one_d_bins = Bins.linear_bins(binning_scheme)
+		lin_bins = []
+
+		for i1 in range(binning_scheme.bin_count):
+			for i2 in range(i1+1):
+				for i3 in range(i2+1):
+					k1 = one_d_bins.bins[i1]
+					k2 = one_d_bins.bins[i2]
+					k3 = one_d_bins.bins[i3]
+
+					if k2.sup + k3.sup > k1.inf:
+						if open_triangles or k2.center() + k3.center() >= k1.center():
+							lin_bins.append(TriangleBin(k1, k2, k3))
+
+		return TriangleBins(lin_bins, binning_scheme.right_open, open_triangles)
+
+
+class OrderedTriangle:
+	def __init__(self, a: float, b: float, c: float):
+		self.sup = max(a, max(b, c))
+		self.inf = min(a, min(b, c))
+		self.med = a + b + c - self.sup - self.inf
+
+	def __add__(self, other: OrderedTriangle) -> OrderedTriangle:
+		return OrderedTriangle(self.sup+other.sup, self.med+other.med, self.inf+other.inf)
+
+	def __mul__(self, other: float) -> OrderedTriangle:
+		assert other > 0
+		return OrderedTriangle(other*self.sup, other*self.med, other*self.inf)
+
+	def __rmul__(self, other):
+		assert other > 0
+		return OrderedTriangle(other*self.sup, other*self.med, other*self.inf)
+
+
+
+class RealSpaceBispectrumEffectiveBinner:
+	def __init__(self, bins: TriangleBins, effective_triangles: list[OrderedTriangle], counts: list[int]):
+		self.bins = bins
+		self.effective_triangles = effective_triangles
+		self.counts = counts
+
+	@staticmethod
+	def new(binning_scheme: BinningScheme, open_triangles: bool = True) -> RealSpaceBispectrumEffectiveBinner:
+
+		available_schemes = [
+			(BinningScheme(1, 1, 64), "effectives/effective_3D_c1.00_dk1.00_n64_leftopen_opentri-true.txt"),
+			(BinningScheme(1.5, 1, 64, right_open=True), "effectives/effective_3D_c1.50_dk1.00_n63_rightopen_opentri-true.txt"),
+			(BinningScheme(0.5, 1, 64, right_open=False), "effectives/effective_3D_c0.50_dk1.00_n64_leftopen_opentri-true.txt")
+		]
+
+		for from_scheme, file_path in available_schemes:
+			if from_scheme.can_be_rebinned_into(binning_scheme):
+				binner = RealSpaceBispectrumEffectiveBinner._from_file(pathlib.Path(__file__).parent.resolve().joinpath(file_path), from_scheme)
+				return binner._rebin(from_scheme, binning_scheme, open_triangles=open_triangles)
+
+		raise InvalidBispectrumBinningSchemeException
+
+	@staticmethod
+	def _from_file(file_path: str, binning_scheme: BinningScheme) -> RealSpaceBispectrumEffectiveBinner:
+		triangle_bins = TriangleBins.linear_bins(binning_scheme)
+		center_1, center_2, center_3, k1_effective, k2_effective, k3_effective, count = loadtxt(file_path, unpack=True)
+		effective_triangles = []
+		counts = []
+		for i, bin in enumerate(triangle_bins.bins):
+			assert bin.center() == (center_1[i], center_2[i], center_3[i]), "The file does not contain triangles for the given binning scheme"
+			effective_triangles.append(OrderedTriangle(k1_effective[i], k2_effective[i], k3_effective[i]))
+			counts.append(count[i])
+
+		binner = RealSpaceBispectrumEffectiveBinner(triangle_bins, effective_triangles, counts)
+		return binner
+
+	def _rebin(self, from_scheme: BinningScheme, to_scheme: BinningScheme, open_triangles: bool = True) -> RealSpaceBispectrumEffectiveBinner:
+		if not from_scheme.can_be_rebinned_into(to_scheme):
+			raise InvalidBispectrumBinningSchemeException
+
+		new_bins = TriangleBins.linear_bins(to_scheme, open_triangles=open_triangles)
+		new_effective = [OrderedTriangle(0, 0, 0)] * len(new_bins.bins)
+		new_counts = [0] * len(new_bins.bins)
+		new_binner = RealSpaceBispectrumEffectiveBinner(new_bins, new_effective, new_counts)
+
+		input_linear_bins = Bins.linear_bins(from_scheme)
+		output_linear_bins = Bins.linear_bins(to_scheme)
+
+		mapping = { b: next((c for c in output_linear_bins.bins if c.contains(b.center())), None) for b in input_linear_bins.bins }
+		triangle_mapping = {}
+		for triangle_input_bin in self.bins.bins:
+			b1 = mapping[triangle_input_bin.bin1]
+			b2 = mapping[triangle_input_bin.bin2]
+			b3 = mapping[triangle_input_bin.bin3]
+			if b1 is not None and b2 is not None and b3 is not None:
+				triangle_mapping[triangle_input_bin] = TriangleBin(b1, b2, b3)
+			else:
+				triangle_mapping[triangle_input_bin] = None
+		bins_to_index_mapping = { output_triangle_bin: i for i, output_triangle_bin in enumerate(new_bins.bins) }
+
+		for j, input_triangle_bin in enumerate(self.bins.bins):
+			output_triangle_bin = triangle_mapping.get(input_triangle_bin)
+			if output_triangle_bin is not None:
+				idx = bins_to_index_mapping.get(output_triangle_bin)
+				if idx is not None:
+					print(j)
+					count = self.counts[j]
+					new_counts[idx] += count
+					new_effective[idx] += count * self.effective_triangles[j]
+
+		for i in range(len(new_binner.bins.bins)):
+			new_effective[i] = new_effective[i] * (1/new_counts[i])
+			print(new_bins.bins[i].bin1, new_bins.bins[i].bin2, new_bins.bins[i].bin3, new_effective[i].sup, new_effective[i].med, new_effective[i].inf, new_counts[i])
+		new_binner.effective_triangles = new_effective
+		new_binner.counts = new_counts
+		return new_binner
+
+class InvalidBispectrumBinningSchemeException(Exception):
+	"The binning scheme selected for the bispectrum is invalid. Only schemes that can be remapped from BinningScheme(0.5, 1, 64) or BinningScheme(1, 1, 64) are valid."
+	pass
